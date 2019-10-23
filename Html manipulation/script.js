ea25547ab9c73cc7c3cd23ef67386cc992a106e2
drewitzmax <drewitz.max@googlemail.com>
2019-10-23 11:40:53 +0200

    finished task

---
+++ Html manipulation/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+   $("img[src^='notebook'").parent().css("background-color","red");
+   $("img[src^='phone'").parent().css("background-color","green");
+   $("img[src^='tablet'").parent().css("background-color","blue");
+
+})
